e5d373713ca84d8ec76c2ac6c2ccb2e72e45a26d
ErwanR-Holberton <6218@holbertonstudents.com>
2024-04-30 17:47:27 +0200

    Update

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, this is a test page!'
+
+
+@app.route('/index')
+def index():
+    return render_template('3.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
