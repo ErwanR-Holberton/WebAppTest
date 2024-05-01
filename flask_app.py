74cd174e194ca7db74ed2f143487c67de7f70ebc
ErwanR-Holberton <6218@holbertonstudents.com>
2024-05-01 18:32:13 +0200

    Add logs about requests

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import geoip2.database
 
 app = Flask(__name__)
+reader = geoip2.database.Reader('GeoLite2-City.mmdb')
+
+def log(message):
+    with open('request_logs.log', 'a') as f:
+        f.write(message + '\n')
+
 
 @app.route('/')
 def hello():
+     # Get the IP address of the client
+    ip_address = request.remote_addr
+
+    # Get approximate location based on IP address
+    try:
+        response = reader.city(ip_address)
+        country = response.country.name
+        city = response.city.name
+        latitude = response.location.latitude
+        longitude = response.location.longitude
+
+        # Log IP address and location information
+        log_message = f"IP Address: {ip_address}, Country: {country}, City: {city}, Latitude: {latitude}, Longitude: {longitude}"
+
+    except geoip2.errors.AddressNotFoundError:
+        log_message = f"IP Address: {ip_address}, Location not found"
+    log(log_message)
     return 'V1'
 
 @app.route('/flan')
