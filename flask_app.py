6bdca82c85f7951c6ecd638a033e70f0fcabf4a4
ErwanR-Holberton <6218@holbertonstudents.com>
2024-05-01 19:51:07 +0200

    log requests

--- flask_app.py
+++ flask_app.py
@@ -1,43 +1,21 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template, request
-import subprocess
-
-# Install geoip2 package using pip
-subprocess.run(["pip3", "install", "geoip2"], check=True)
-
-
-import geoip2.database
 
 app = Flask(__name__)
-reader = geoip2.database.Reader('GeoLite2-City.mmdb')
 
 def log(message):
     with open('request_logs.log', 'a') as f:
-        f.write(message + '\n')
+        f.write(message + "\n")
 
-print("server start")
+@app.before_request
+def log_request_info():
+    try:
+        log(str(request))
+    except Exception as e:
+        log(e)
 
 @app.route('/')
 def hello():
-    print("root call")
-     # Get the IP address of the client
-    ip_address = request.remote_addr
-
-    # Get approximate location based on IP address
-    try:
-        response = reader.city(ip_address)
-        country = response.country.name
-        city = response.city.name
-        latitude = response.location.latitude
-        longitude = response.location.longitude
-
-        # Log IP address and location information
-        log_message = f"IP Address: {ip_address}, Country: {country}, City: {city}, Latitude: {latitude}, Longitude: {longitude}"
-
-    except geoip2.errors.AddressNotFoundError:
-        log_message = f"IP Address: {ip_address}, Location not found"
-    log(log_message)
-    print("no error")
     return 'V1'
 
 @app.route('/flan')
