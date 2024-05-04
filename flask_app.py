166b232d0874d5c762e409bd396c1653418b6836
ErwanR-Holberton <6218@holbertonstudents.com>
2024-05-04 14:18:12 +0200

    Add flutter app

--- flask_app.py
+++ flask_app.py
@@ -5,17 +5,42 @@ app = Flask(__name__)
 
 def log(message):
     with open('request_logs.log', 'a') as f:
+        print(message)
         f.write(message + "\n")
 
 @app.before_request
 def log_request_info():
     try:
-        log(str(request))
+        log_message = "Request Info: "
+        """log(f"Request: {str(request)}, Method: {request.method}, URL: {request.url}, Headers: {request.headers}, Remote Address: {request.remote_addr}, Form Data: {request.form}, Query Parameters: {request.args}, JSON Data: {request.json}, Cookies: {request.cookies}, Files: {request.files}, Path: {request.path}, Full Path: {request.full_path}, Scheme: {request.scheme}, Base URL: {request.base_url}, User Agent: {request.user_agent}")
+        """
+        log(request.url)
+        log(request.headers)
+        log(request.remote_addr)
+        log(request.form)
+        log(request.method)
+
+        """for attr in dir(request):                               # Iterate over attributes of the request object
+
+            if not attr.startswith('_'):                        # Skip private attributes and methods
+
+
+                if attr == 'json':
+                    continue
+                value = getattr(request, attr)                  # Get the value of the attribute
+                if value is None:
+                    continue
+                print(attr, value)
+                log_message += "{}: {},".format(attr, value)    # Add the attribute and its value to the log message
+
+        log(str(log_message))"""
+
     except Exception as e:
-        log(e)
+        log(str(e))
 
 @app.route('/')
 def hello():
+    print("hello")
     return 'V1'
 
 @app.route('/flan')
