6c13d569d2ccfe027ad8e6662e0f6f0c35d42ec9
ErwanR-Holberton <6218@holbertonstudents.com>
2024-07-09 21:30:40 +0200

    Added DB api

---
+++ Apps/DB.py
@@ -0,0 +1,38 @@
+from FlaskApp import *
+prefix = "/DB/"
+from DB_CONFIG import MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST, MYSQL_DATABASE
+
+db_config = {
+    'user': MYSQL_USER,
+    'password': MYSQL_PASSWORD,
+    'host': MYSQL_HOST,
+    'database': MYSQL_DATABASE,
+    'port': 3306,
+}
+
+def routes():
+    @app.route(prefix)
+    def indexDB():
+        return "Nothing to see, it's a DB"
+
+    @app.route(prefix + "PUT")
+    def DB_PUT():
+        try:
+            connection = mysql.connector.connect(**db_config)
+            cursor = connection.cursor()
+
+            query = "SELECT word, vector FROM words;"
+            cursor.execute(query)
+
+            words = cursor.fetchall()
+
+            cursor.close()
+            connection.close()
+
+            # Render a template to display the words
+            return render_template('words.html', words=words)
+
+        except mysql.connector.Error as err:
+            return f"Error accessing MySQL: {err}"
+
+
