2f392676b66fe32f13926678ad6b3e83a97c0527
ErwanR-Holberton <124136642+ErwanR-Holberton@users.noreply.github.com>
2024-08-24 14:45:35 +0000

    Add security: unique names in player table

--- Apps/Babyfoot.py
+++ Apps/Babyfoot.py
@@ -30,23 +30,29 @@ def Create_Player(username):
     connection = mysql.connector.connect(**db_config)
     cursor = connection.cursor()
 
+    check_query = "SELECT COUNT(*) FROM Player WHERE username = %s;"
+    cursor.execute(check_query, (username))
+    count = cursor.fetchone()[0]
 
-    query = "INSERT INTO Player (username) VALUES (%s);"    # Prepared statement to avoid SQL injection
+    if count > 0:
+        result = "Player with this username already exists!"
+    else:
+        query = "INSERT INTO Player (username) VALUES (%s);"    # Prepared statement to avoid SQL injection
 
-    try:
-    
-        cursor.execute(query, (username,))    # Execute the query with the provided username
+        try:
         
-    
-        connection.commit()    # Commit the transaction to the database
-        result = "Player added successfully!"
-    except mysql.connector.Error as err:
-        result = f"Error: {err}"
-        connection.rollback()  # Rollback in case of an error
-    finally:
-        # Always close the cursor and connection
-        cursor.close()
-        connection.close()
+            cursor.execute(query, (username))    # Execute the query with the provided username
+            
+        
+            connection.commit()    # Commit the transaction to the database
+            result = "Player added successfully!"
+        except mysql.connector.Error as err:
+            result = f"Error: {err}"
+            connection.rollback()  # Rollback in case of an error
+        finally:
+            # Always close the cursor and connection
+            cursor.close()
+            connection.close()
     return result
 
 def routes():
