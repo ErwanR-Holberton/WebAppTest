bc25a3a18bf37c76a98216004b6e2531971c96a4
ErwanR-Holberton <124136642+ErwanR-Holberton@users.noreply.github.com>
2024-08-25 18:02:23 +0000

    forgot dots

--- Apps/Babyfoot.py
+++ Apps/Babyfoot.py
@@ -42,7 +42,7 @@ def check_players(players_to_check):
         cursor.execute(query, players_to_check)
         count = cursor.fetchone()[0]    # Fetch the count of matching items
         
-        if count != len(players_to_check) # Check if all players are in the list
+        if count != len(players_to_check): # Check if all players are in the list
             return count
         return True
     finally:
