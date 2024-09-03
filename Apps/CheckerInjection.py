05aac4cc8826b3559eee218ead239556746279e2
ErwanR-Holberton <124136642+ErwanR-Holberton@users.noreply.github.com>
2024-09-03 14:31:07 +0000

    Added Checker Page

---
+++ Apps/CheckerInjection.py
@@ -0,0 +1,16 @@
+if __name__ != "__main__":
+    from FlaskApp import *
+prefix = "/CheckerInjection/"
+
+data = []
+
+
+def routes():
+    @app.route(prefix)
+    def index_CheckerInjection():
+        return render_template(prefix + 'index.html', data=data)
+
+    @app.route(prefix + 'send', methods=['POST'])
+    def send_data_CheckerInjection():
+        data.append(request.data)
+        return "OK"
