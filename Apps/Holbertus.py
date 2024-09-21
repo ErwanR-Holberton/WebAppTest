6578be54579b47ad4fb451833c2de66ab5f9c01a
ErwanR-Holberton <6218@holbertonstudents.com>
2024-09-21 17:17:13 +0200

    Added route for word check

--- Apps/Holbertus.py
+++ Apps/Holbertus.py
@@ -4,11 +4,15 @@ import os, random, string, base64
 prefix = "/Holbertus/"
 
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-file_path = os.path.join(base_dir, 'Tusmo.txt')
+file_path = os.path.join(base_dir, 'ods6.txt')
 
-def load_motus_words():
+def load_motus_words(min_length=5, max_length=50):
     with open(file_path, "r") as file:
-        return [line.strip() for line in file.readlines() if all(c in string.ascii_uppercase for c in line.strip())]
+        return [
+            line.strip() 
+            for line in file.readlines() 
+            if min_length <= len(line.strip()) <= max_length
+        ]
 
 def pick_word():
     return random.choice(load_motus_words())
@@ -21,7 +25,18 @@ def routes():
     @app.route(prefix + "mot_du_jour")
     def holbertus_mot_du_jour():
         word = pick_word()
-        print(word)
         coded_word = base64.b64encode(word.encode()).decode('utf-8')
         return render_template(prefix + 'word.html', word=word, coded_word=coded_word)
+    
+
+    @app.route(prefix + "check_word", methods=['POST'])
+    def holbertus_check_word():
+        word = request.get_json().get('word')
+        words = load_motus_words()
+        if word in words:
+            return "OK"
+        else:
+            return f"{word} not in dictionary"
+    
+
 
